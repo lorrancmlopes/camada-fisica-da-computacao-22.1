1a08602eb315fa2a03a3865b9f5c962bbf027e3a
Lorran Caetano Machado Lopes <lorrancml@al.insper.edu.br>
2022-05-25 15:01:19 -0300

    toques finais + meme.wav + imgs dos graficos

--- P8/decoder.py
+++ P8/decoder.py
@@ -5,11 +5,10 @@ from funcoes_LPF import filtro
 import sounddevice as sd
 
 def decoder():
-
-
     sd.default.samplerate =  44100 
     sd.default.channels = 1  #voce pode ter que alterar isso dependendo da sua placa
-    duration = 3  #tempo em segundos que ira aquisitar o sinal acustico captado pelo mic
+    print("_____________ D E C O D E R _____________\n\n")
+    print("Lendo o arquivo...")
     audio, samplerate = sf.read('ModularizadoNormalizado.wav')
 
     meuSinal = signalMeu() 
@@ -18,41 +17,39 @@ def decoder():
     #Verifique que o sinal recebido tem a banda dentro de 10.500 Hz e 15.500 Hz (faça o Fourier).
     xf, yf = meuSinal.calcFFT(audio, samplerate) ## Calcula o Fourier do sinal audio.
 
-    
-
-    #Demodule o áudio enviado pelo seu colega.
-    print("Gerando senoide portadora")
+    #Demodule o áudio 
+    print("Gerando senoide portadora...")
     Amplitude = 1
-    T = 3 #segundos
+    T = 5 #segundos
     fs = 44100
         # essa senoide tem que ter taxa de amostragem de 44100 amostras por segundo
     freqPortadora = 13000 # 13000 Hz
     x1, sPortadora = meuSinal.generateSin(freqPortadora, Amplitude, T, 44100)
     sinalDemodulado = sPortadora*audio
-    #signalMeu.plotFFT(meuSinal, sinalDemodulado, fs)
 
     #Filtre as frequências superiores a 2.500 Hz.
+    print("Filtrando as frequências cima de 2.500 Hz...")
     cutOff = 2500 #2500 Hz
     audioFiltrado = filtro(sinalDemodulado, fs, cutOff)
 
     #Execute o áudio do sinal demodulado e verifique que novamente é audível. 
-    print("Audio demodulado")
+    print("Reproduzindo o aúdio demodulado (audível novamente)...")
     sd.play(audioFiltrado, fs)
     sd.wait()
 
-
-###########################
-    figure, axis = plt.subplots(2,1)
+    #################### G R A F I C O S ####################
+    figure, axis = plt.subplots(3,1)
     axis[0].plot(xf,yf)
-    axis[0].title.set_text('Sinal demodulado FTT')
-    axis[0].set_xlim(10500,15500)
-    plt.tight_layout()
+    axis[0].title.set_text('Sinal Recebido')
 
+    xf, yf = meuSinal.calcFFT(sinalDemodulado, samplerate) ## Calcula o Fourier do sinal demodulado.
+    axis[1].plot(xf,yf)
+    axis[1].title.set_text('Sinal Demodulado FTT')
 
     xf, yf = meuSinal.calcFFT(audioFiltrado, samplerate) ## Calcula o Fourier do sinal audio.
-    axis[1].plot(xf,yf)
-    axis[1].title.set_text('Sinal demodulado e Filtrado FTT')
-    #axis[1].set_xlim(10500,15500)
+    axis[2].plot(xf,yf)
+    axis[2].title.set_text('Sinal Demodulado e Filtrado FTT')
+
     plt.tight_layout()
     plt.show()
 
--- P8/encoder.py
+++ P8/encoder.py
@@ -1,13 +1,10 @@
-from turtle import shape
 from suaBibSignal import signalMeu
 import time
 import sounddevice as sd
 import numpy as np
 import matplotlib.pyplot as plt
-import peakutils
 from funcoes_LPF import filtro
 from scipy.io.wavfile import write
-import wave as wv
 import soundfile as sf
 
     # function to normalize array
@@ -16,11 +13,10 @@ def normalize(item):
     return (item)/(np.max(np.abs(item)))
 
 
-def main():
+def encoder():
+    print("_____________ D E C O D E R _____________\n\n")
     # 1. Faça a leitura de um arquivo de áudio .wav de poucos segundos (entre 2 e 5) 
     #    previamente gravado com uma taxa de amostragem de 44100 Hz.
-
-
 
         #declare um objeto da classe da sua biblioteca de apoio (cedida)
     meuSinal = signalMeu() 
@@ -31,33 +27,39 @@ def main():
     
     sd.default.samplerate =  44100 
     sd.default.channels = 1  #voce pode ter que alterar isso dependendo da sua placa
-    duration = 3  #tempo em segundos que ira aquisitar o sinal acustico captado pelo mic
-
-
-        # faca um printo na tela dizendo que a captacao comecará em n segundos. e entao 
+    duration = 5  #tempo em segundos que ira aquisitar o sinal acustico captado pelo mic
+        # faca um print na tela dizendo que a captacao comecará em n segundos. e entao 
         #use um time.sleep para a espera
-    def countdown(num_of_secs):
-        while num_of_secs:
-            s = num_of_secs
-            min_sec_format = 'A captura de som começará em {:02d} segundos'.format(s)
-            print(min_sec_format, end='\n')
-            time.sleep(1)
-            num_of_secs -= 1
-    countdown(duration)
-    # print('Countdown finished.')
-   
-    #faca um print informando que a gravacao foi inicializada
-    print("Começou!")
-        #declare uma variavel "duracao" com a duracao em segundos da gravacao. poucos segundos ... 
-        #calcule o numero de amostras "numAmostras" que serao feitas (numero de aquisicoes)
+
+
     numAmostras = fs*duration#duvida
     freqDeAmostragem = fs
-    audio = sd.rec(int(numAmostras), freqDeAmostragem, channels=1)
-    sd.wait()
-    print("...     FIM")
+    modoAudio = input("Você deseja usar o meme ou gravar um aúdio? \n Digite 1 para 'meme' ou 2 para gravar agora: ")
+    if modoAudio == '1':
+        memePath = 'meme.wav'
+        audio, samplerate = sf.read(memePath)
+
+    
+    else:
+        def countdown(num_of_secs):
+            while num_of_secs:
+                s = num_of_secs
+                min_sec_format = 'A captura de som começará em {:02d} segundos'.format(s)
+                print(min_sec_format, end='\r')
+                time.sleep(1)
+                num_of_secs -= 1
+        countdown(duration)
+        #faca um print informando que a gravacao foi inicializada
+        print("\Começou!")
+            #declare uma variavel "duracao" com a duracao em segundos da gravacao. poucos segundos ... 
+            #calcule o numero de amostras "numAmostras" que serao feitas (numero de aquisicoes)
+        
+        audio = sd.rec(int(numAmostras), freqDeAmostragem, channels=1)
+        sd.wait()
+        print("...     FIM")
     
         #analise sua variavel "audio". pode ser um vetor com 1 ou 2 colunas, lista ...
-    #print(f"Audio: {audio}")
+    print(f"Audio: {audio}")
     #grave uma variavel com apenas a parte que interessa (dados)
     y = audio[:,0]
      # 2. Filtre e elimine as frequências acima de 2500 Hz.
@@ -82,7 +84,7 @@ def main():
 
     print("Gerando senoide portadora")
     Amplitude = 1
-    T = 3 #segundos
+    T = 5 #segundos
     fs = 44100
         # essa senoide tem que ter taxa de amostragem de 44100 amostras por segundo
     freqPortadora = 13000 # 13000 Hz
@@ -131,11 +133,7 @@ def main():
     #criando arquivo de áudio
     sf.write('ModularizadoNormalizado.wav', sinalModuladoNormalizado, fs)
 
-
-
-
-
-main()
+encoder()
 
 
     
